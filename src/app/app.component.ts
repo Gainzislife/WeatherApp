37b186599e6914df5ca6c11a71fec021fa3cdbcb
Jaco Steyn <jacosteyn1990@gmail.com>
2020-07-28 11:59:05 +0200

    Second commit
    Added base app html, css and ts
    Added home component
    Added ui.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { UiService } from './services/ui.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'WeatherApp';
+  showMenu = false;
+  darkModeActive: boolean;
+
+  constructor(public router: Router, public ui: UiService) {
+  }
+
+  sub1;
+
+  ngOnInit() {
+    this.sub1 = this.ui.darkModeState.subscribe((value) => {
+      this.darkModeActive = value;
+    });
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.ui.darkModeState.next(!this.darkModeActive);
+  }
+
+  ngOnDestroy() {
+    this.sub1.unsubscribe();
+  }
 }
